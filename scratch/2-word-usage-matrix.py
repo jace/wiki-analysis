c9352870aba4578bed9cd74fb9a3c88100e24335
jackerhack <jackerhack@246420f6-f761-11dd-99c0-75d572ba1ddd>
2009-03-17 16:26:40 +0000

    Script 2: analysis by words added or deleted from the given page over a given date range. The Python mwclient library has a bug that causes this script to fail if there are more than 50 revisions in the time period. I've filed a bug report with the developer.

---
+++ scratch/2-word-usage-matrix.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# -*- coding: utf-8  -*-
+"""
+For a given Wikipedia article and time period, this script looks up all
+the changes introduced by each editor and tallies what words they used or
+removed.
+"""
+
+# TODO:
+# 1. Accept start/end date ranges. Parse strings in the yyyy-mm-dd format.
+# 2. Test mwclient's start/end specification for revisions.
+# 3. Ask mwclient to get full content of pages and in old-to-new sequence.
+# 4. Keep track of previous revision content.
+# 5. Remove code related to other pages by the same editor.
+# 6. Perform a diff to get exact words added or removed. Tricky bit.
+# 7. Sort words and use counts (number added, number removed, separately!)
+# 8. Save to CSV(s).
+
+import sys
+from optparse import OptionParser
+import datetime
+import csv
+import mwclient
+from worddiff import worddiff, striptags
+
+MWDATEFMT = '%Y-%m-%dT%H:%M:%SZ'
+
+class IgnoreEditor(Exception):
+    pass
+
+def parsedate(text):
+    if text == 'now':
+        return datetime.datetime.utcnow()
+    elif text == 'today':
+        return datetime.datetime.today()
+    elif text == 'yesterday':
+        return datetime.datetime.today() - datetime.timedelta(days=1)
+    else:
+        try:
+            return datetime.datetime.strptime(text, '%Y-%m-%d')
+        except ValueError:
+            try:
+                return datetime.datetime.strptime(text, '%Y/%m/%d')
+            except ValueError:
+                try:
+                    return datetime.datetime.strptime(text, '%d-%m-%Y')
+                except ValueError:
+                    return datetime.datetime.strptime(text, '%d/%m/%Y')
+
+def analyze(article, start, end, wiki, ignore, outfile):
+    start = parsedate(start)
+    end = parsedate(end)
+
+    print "Connecting to %s..." % wiki
+    site = mwclient.Site(wiki)
+    print "Looking up revisions between %s and %s." % (
+        start.strftime(MWDATEFMT), end.strftime(MWDATEFMT))
+    print 'Revisions for "%s":' % article,
+    sys.stdout.flush()
+    page = site.Pages[article]
+    if 0: assert isinstance(page, mwclient.page.Page)
+
+    # Place all editors in a dictionary
+    editors = {} # Dictionary of word: (add count, remove count)
+    allwords = set() # Set of all words found across editors
+
+    revisions = page.revisions(start=start.strftime(MWDATEFMT),
+                               end=end.strftime(MWDATEFMT),
+                               dir='newer',
+                               prop='ids|timestamp|flags|comment|user|content')
+
+    # This is a typical revision (from the Bangalore page):
+    # {u'comment': u'/* Culture and education */',
+    #  u'timestamp': (2006, 1, 3, 4, 10, 37, 1, 3, -1),
+    #  u'anon': u'', u'revid': 33686266, u'user': u'59.92.138.207'}
+
+    try:
+        revision_count = 0
+        rev = revisions.next()
+        oldcontent = striptags(rev[u'*'])
+        while True:
+            print '\rRevisions for "%s": %d on %s' % (article, revision_count,
+                '%04d/%02d/%02d %02d:%02d:%02d' % rev[u'timestamp'][:6]),
+            sys.stdout.flush()
+            rev = revisions.next() # Use first revision only as reference point
+            revision_count += 1
+            # Ignore editors we've been asked to
+            if rev[u'user'] not in ignore:
+                editorstats = editors.get(rev[u'user'], {})
+                # Analyse content of this edit for words added and removed
+                newcontent = striptags(rev[u'*'])
+                for change, word in worddiff(oldcontent, newcontent):
+                    allwords.add(word)
+                    wordstats = editorstats.get(word, (0, 0))
+                    if change == '+':
+                        editorstats[word] = (wordstats[0] + 1, wordstats[1])
+                    elif change == '-':
+                        editorstats[word] = (wordstats[0], wordstats[1] + 1)
+                editors[rev[u'user']] = editorstats
+            # Done. On to next revision.
+            oldcontent = newcontent
+    except StopIteration:
+        pass
+
+    print
+
+    editor_names = editors.keys()
+    editor_names.sort() # Will make table generation easier further down
+    print "Found %d editors with %d words: %s" % (len(editor_names), len(allwords),
+        u', '.join(editor_names).encode('utf-8'))
+
+    if outfile:
+        print "Saving table to %s..." % outfile
+        out = csv.writer(open(outfile, 'wb'))
+        if 0: assert isinstance(out, csv.DictWriter)
+        out.writerow(['Word/Editor'] + [e.encode('utf-8') for e in editor_names])
+        sorted_words = list(allwords)
+        sorted_words.sort()
+        for word in sorted_words:
+            row = [editors[e].get(word, (0,0)) for e in editor_names]
+            out.writerow([word.encode('utf-8')]+row)
+        print "All Done."
+
+def main(argv):
+    '''%prog [options] "Article Name"'''
+    parser = OptionParser(usage=main.__doc__)
+    parser.add_option('-s', '--start',
+                      default=(datetime.datetime.utcnow() - datetime.timedelta(weeks=4)).strftime('%Y-%m-%d'),
+                      help='Starting date [default %default]')
+    parser.add_option('-e', '--end', default='now',
+                      help="Ending date (or one of now/today/yesterday) [default %default]")
+    parser.add_option('-w', '--wiki', type='string', default='en.wikipedia.org',
+                      help='MediaWiki site to look up [default %default]')
+    parser.add_option('-i', '--ignore', type='string', action='append', default=[],
+                      help='Ignore edits by this editor'\
+                      ' (useful to filter maintenance bots)')
+    parser.add_option('-o', '--output', type='string', default=None,
+                      help='CSV file to save output to')
+    (options, parms) = parser.parse_args(argv)
+
+    if len(parms) < 1:
+        print >> sys.stderr, "Article must be specified."
+        return 1
+    elif len(parms) > 1:
+        print >> sys.stderr, "Only one article name may be specified."
+        return 1
+
+    return analyze(parms[0], options.start, options.end, options.wiki,
+                   options.ignore, options.output)
+
+
+if __name__=='__main__':
+    sys.exit(main(sys.argv[1:]))
---
+++ scratch/worddiff.py
@@ -0,0 +1,118 @@
+#/usr/bin/env python
+# -*- coding: utf-8  -*-
+
+"""
+Word Differ
+===========
+
+Returns words that have been added or removed between two pieces of text.
+"""
+
+__author__ = 'Kiran Jonnalagadda <jace@pobox.com>'
+
+import re
+import difflib
+
+# Ignore common punctuation but notably not apostropes, straight or curved.
+IGNORECHARS=u' \t,./<>?[]{}\\|`~!@#$%^&*()-_=+;:"\r\n'\
+            u'«»¡¿¢£¤¥¦§¬¶·×÷         ​‌‐‑‒–—―‘‚‛“”„‟‹›‼‽‾'
+
+def _makecharregex(chars):
+    result = chars.replace('\\', '\\\\').replace(
+        '[', '\\[').replace(']', '\\]').replace('.', '\\.').replace(
+        '?', '\\?').replace('*', '\\*').replace('-', '\\-').replace(
+        '^', '\\^')
+    result = '[' + result + ']+'
+    return result
+
+resplit = re.compile(_makecharregex(IGNORECHARS))
+resplit = re.compile('\W+') # XXX: Apostrophe eating splitter
+retag = re.compile('<.*?>')
+
+def striptags(text):
+    """
+    Remove HTML/XML tags from text, inserting spaces in their place::
+
+      >>> striptags('<h1>title</h1>')
+      ' title '
+      >>> striptags('plain text')
+      'plain text'
+      >>> striptags(u'word<br>break')
+      u'word break'
+    """
+    return retag.sub(' ', text)
+
+def getwords(text, splitchars=resplit):
+    """
+    Get words in text by splitting text along specified splitchars
+    and stripping out the splitchars::
+
+      >>> getwords('this is some text.')
+      ['this', 'is', 'some', 'text']
+      >>> getwords('and/or')
+      ['and', 'or']
+      >>> getwords('one||two')
+      ['one', 'two']
+      >>> getwords("does not is doesn't")
+      ['does', 'not', 'is', "doesn't"]
+      >>> getwords(u'hola unicode!')
+      [u'hola', u'unicode']
+      >>> getwords('^break^me^', splitchars='^')
+      ['break', 'me']
+
+    It appears Python's doctest module defaults to Latin-1 even if the file
+    encoding specifies UTF-8, so we can't run a test in here with non-ASCII
+    characters. Suggestions on how to do this are welcome.
+    """
+    if isinstance(splitchars, basestring):
+        splitchars = re.compile(_makecharregex(splitchars))
+
+    # We can't simply use '\W+' because that will split along the apostrophe.
+    result = splitchars.split(text)
+    # Blank tokens will only be at beginning or end of text.
+    if result[0] == '':
+        result.pop(0)
+    if result[-1] == '':
+        result.pop(-1)
+    return result
+
+def worddiff(text1, text2):
+    """
+    Generator that returns words that have been added or removed.
+    Ignores specified junk characters (default: common latin punctuation).
+
+    Examples::
+
+      >>> list(worddiff('original text', 'changed text'))
+      [('-', 'original'), ('+', 'changed')]
+
+    Words separated by markers are correctly identified::
+
+      >>> list(worddiff('this and that', 'this and/or that'))
+      [('+', 'or')]
+
+    Changes in only punctuation are ignored::
+
+      >>> list(worddiff('some text', 'some text.'))
+      []
+
+    """
+    for item in difflib.ndiff(getwords(text1), getwords(text2)):
+        if item.startswith('+ '):
+            yield ('+', item[2:])
+        elif item.startswith('- '):
+            yield ('-', item[2:])
+
+if __name__=='__main__':
+    import sys
+    if '-t' in sys.argv[1:]:
+        import doctest
+        doctest.testmod()
+    else:
+        if len(sys.argv) != 3:
+            print "Syntax: %s file1 file2" % sys.argv[0]
+        else:
+            text1 = open(sys.argv[1], 'rb').read()
+            text2 = open(sys.argv[2], 'rb').read()
+            for result in worddiff(text1, text2):
+                print result
